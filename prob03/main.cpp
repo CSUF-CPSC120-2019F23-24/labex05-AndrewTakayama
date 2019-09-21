ac541415c56182392a2cc8edcae06dbcb83b387c
AndrewTakayama <Takayamaa19@avphs.net>
2019-09-21 14:33:44 -0700

    I created a program which made a random rectangle each time.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,33 @@
-// Random Rectangle
+//Random rectangle
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int main ()
+{
+  //Sets the values to the length and height of the rectangle
+  const int HEIGHT = 3;
+  int MAX = 40;
+
+  //Randomizes the amount of hashtags for the recangle.
+  unsigned seed = time(0);
+  srand(seed);
+  int length = rand();
+  length = rand() % MAX + 1;
+
+
+  //Actually displays the rectangle
+  std::string rectangle;
+  rectangle.assign(length,'#');
+
+  std::cout << rectangle << std::endl;
+  std::cout << rectangle << std::endl;
+  std::cout << rectangle << std::endl;
+
+
+
+
+
+return 0;
+}
